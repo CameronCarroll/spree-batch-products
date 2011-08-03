cd534f688d775daaa35ea50644c5f9aec61aa35d
Cameron Carroll <ckcarroll4@gmail.com>
2011-08-02 17:47:57 -0700

    Refactored datasheet model; Added variant creation

--- app/models/product_datasheet.rb
+++ app/models/product_datasheet.rb
@@ -1,42 +1,55 @@
 class ProductDatasheet < ActiveRecord::Base
   require 'spreadsheet'
+
+has_attached_file :xls, :path => ":rails_root/uploads/product_datasheets/:id/:basename.:extension"
+
+validates_attachment_presence :xls
+validates_attachment_content_type :xls, :content_type => ['application/vnd.ms-excel','text/plain']
+
+scope :not_deleted, where("product_datasheets.deleted_at is NULL")
+scope :deleted, where("product_datasheets.deleted_at is NOT NULL")
+
+def path
+  "#{Rails.root}/uploads/product_datasheets/#{self.id}/#{self.xls_file_name}"
+end
+
+#// Main method for control of spreadsheet processing:
+#// Opens up a workbook and its two worksheets. The first sheet is used to define products,
+#// and the second sheet is used to define variants.
+
+#// A note on dimensions: values 0-3 of the return array contain the first used column, and the first
+#// unused column. See documentation at http://spreadsheet.rubyforge.org/Spreadsheet/Worksheet.html
+def process
+  uploaded_workbook = Spreadsheet.open self.path
+
+product_sheet = uploaded_workbook.worksheet(0)
+product_columns = [product_sheet.dimensions[2], product_sheet.dimensions[3]]
+product_headers = product_sheet.row(0)
+
+if not uploaded_workbook.worksheet(1).nil?
+  variant_sheet = uploaded_workbook.worksheet(1)
+  variant_columns = [variant_sheet.dimensions[2], variant_sheet.dimensions[3]]
+  variant_headers = variant_sheet.row(0)
+end
+
+@records_matched = 0
+@records_updated = 0
+@records_failed = 0
+@failed_queries = 0
+end #process
   
-  has_attached_file :xls, :path => ":rails_root/uploads/product_datasheets/:id/:basename.:extension"  
-  
-  validates_attachment_presence :xls
-  validates_attachment_content_type :xls, :content_type => ['application/vnd.ms-excel','text/plain']
-  
-  scope :not_deleted, where("product_datasheets.deleted_at is NULL")
-  scope :deleted, where("product_datasheets.deleted_at is NOT NULL")
-  
-  def path
-    "#{Rails.root}/uploads/product_datasheets/#{self.id}/#{self.xls_file_name}"
-  end
-  
-  def perform
-    workbook = Spreadsheet.open self.path
-    worksheet = workbook.worksheet(0)
-    columns = [worksheet.dimensions[2], worksheet.dimensions[3]]
-    headers = worksheet.row(0)
-    @records_matched = 0
-    @records_updated = 0
-    @records_failed = 0
-    @failed_queries = 0
-    
-    
+  def perform(sheet)
+
     #// Passing 1 into each(1) defines how many rows to skip before processing the spreadsheet.
     #// Since the first row is already defined in headers=worksheet.row(0), we start at the data.
     worksheet.each(1) do |row|
       attr_hash = {}
-      
-      #// Uses pre-defined headers array and associates each header value with its target value.
-      for i in columns[0]..columns[1]
-        attr_hash[headers[i]] = row[i] unless row[i].nil?
-      end
-      
-      #// Check for empty ID, which signifies record creation:
+      load_headers(row)
+
+      #// Checks first header value for a blank 'id', which signifies record creation.
       #// If record is to be created, checks for product_id column, which signifies variant creation.
-      if headers[0] == 'id' && row[0].nil? && headers[1] == 'product_id' && row[1].nil?
+      
+      if headers[0] == 'id' && row[0].nil? && headers[1] == 'product_id' && !row[1].nil?
         create_variant(attr_hash)
       elsif headers[0] == 'id' && row[0].nil?
         create_product(attr_hash)
@@ -57,14 +70,31 @@ class ProductDatasheet < ActiveRecord::Base
     self.update_attributes(attr_hash)
   end
   
+   #// Uses pre-defined headers array and associates each header value with its target value.
+  #// Iterates between the first used and the first unused columns and grabs the header and row value.  
+  def load_headers(row)
+    for i in columns[0]..columns[1]
+     attr_hash[headers[i]] = row[i] unless row[i].nil?
+    end
+  end
   
-  
+  #// Simply instantiates a new product using the attribute hash formed in load_headers
   def create_product(attr_hash)
     new_product = Product.new(attr_hash)
     @failed_queries = @failed_queries + 1 if not new_product.save
   end
   
+  
+  #// create_variant uses product_id in attr_hash:
+  #// accepts string, integer values (string for lookup, integer for direct association.)
+  #// If product is found, injects its ID into attr_hash in place of name
   def create_variant(attr_hash)
+    product_to_reference = Product.where(headers[1] => attr_hash[headers[1]])
+    if not product_to_reference.nil?
+      attr_hash[headers[1]] = product_to_reference.id
+    else
+      #Fail miserably :/
+    end
     new_variant = Variant.new(attr_hash)
     @failed_queries = @failed_queries + 1 if not new_product.save
   end
@@ -73,11 +103,11 @@ class ProductDatasheet < ActiveRecord::Base
     products_to_update = Product.where(key => value).all
     @records_matched = @records_matched + products_to_update.size
     products_to_update.each { |product| 
-                                        if product.update_attributes attr_hash 
-                                          @records_updated = @records_updated + 1
-                                        else
-                                          @records_failed = @records_failed + 1
-                                        end }
+        if product.update_attributes attr_hash
+            @records_updated = @records_updated + 1
+        else
+            @records_failed = @records_failed + 1
+         end }
     @failed_queries = @failed_queries + 1 if products_to_update.size == 0
   end
   
@@ -85,11 +115,11 @@ class ProductDatasheet < ActiveRecord::Base
     variants_to_update = Variant.where(key => value).all
     @records_matched = @records_matched + variants_to_update.size
     variants_to_update.each { |variant| 
-                                        if variant.update_attributes attr_hash
-                                          @records_updated = @records_updated + 1
-                                        else
-                                          @records_failed = @records_failed + 1
-                                        end }
+        if variant.update_attributes attr_hash
+           @records_updated = @records_updated + 1
+        else
+           @records_failed = @records_failed + 1
+        end }
     @failed_queries = @failed_queries + 1 if variants_to_update.size == 0
   end
   
