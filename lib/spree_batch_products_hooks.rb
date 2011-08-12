3a6e2329d61e81796e0d9fafc04e609afa8f481d
Cameron Carroll <ckcarroll4@gmail.com>
2011-08-12 16:11:44 -0700

    Fixup hook

--- lib/spree_batch_products_hooks.rb
+++ lib/spree_batch_products_hooks.rb
@@ -2,7 +2,7 @@ class BatchProductsHooks < Spree::ThemeSupport::HookListener
   # custom hooks go here
   Deface::Override.new(:virtual_path => "admin/shared/_product_sub_menu",
                      :name => "batch_products_admin_product_sub_tabs",
-                     :insert_bottom => "[data-hook='admin_product_sub_tabs'], #admin_product_sub_tabs[data-hook]",
+                     :insert_bottom => "ul#sub_nav",
                      :text => "<%= tab(:product_datasheets, :label => :batch_updates) %>",
                      :disabled => false)
 end
